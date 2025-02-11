1233a8d490c7b62062000e2cd6cbc47d31d3efe1
Trisha <johndoe@example.com>
2025-02-12 01:36:31 +0600

    changed Time

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-let romeCityDate = document.querySelector(".date");
-let romeTzTime = moment().tz("Europe/Rome");
+function countryDateTime() {
+  let romeCityDate = document.querySelector(".date");
+  let romeTzTime = moment().tz("Europe/Rome");
 
-romeCityDate.innerHTML = romeTzTime.format("dddd, MMMM Do, YYYY");
+  romeCityDate.innerHTML = romeTzTime.format("dddd, MMMM Do, YYYY");
 
-let romeCityActualTime = document.querySelector(".time");
+  let romeCityActualTime = document.querySelector(".time");
 
-romeCityActualTime.innerHTML = romeTzTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  romeCityActualTime.innerHTML = romeTzTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisCityDate = document.querySelector("#par");
+  let parisTzTime = moment().tz("Europe/Paris");
+
+  parisCityDate.innerHTML = parisTzTime.format("dddd, MMMM Do, YYYY");
+
+  let parisActualTime = document.querySelector("#pid");
+  parisActualTime.innerHTML = parisTzTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+countryDateTime();
+setInterval(countryDateTime, 1000);
