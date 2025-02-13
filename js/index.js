453a5427c8fd84eabef7a27f79d825468a2b972b
Trisha <johndoe@example.com>
2025-02-14 01:27:41 +0600

    updated functions

--- js/index.js
+++ js/index.js
@@ -1,25 +1,53 @@
 function countryDateTime() {
-  let romeCityDate = document.querySelector(".date");
-  let romeTzTime = moment().tz("Europe/Rome");
+  let romCt = document.querySelector("#rome");
 
-  romeCityDate.innerHTML = romeTzTime.format("dddd, MMMM Do, YYYY");
+  if (romCt) {
+    let romeCityDate = document.querySelector(".date");
+    let romeTzTime = moment().tz("Europe/Rome");
 
-  let romeCityActualTime = document.querySelector(".time");
+    romeCityDate.innerHTML = romeTzTime.format("dddd, MMMM Do, YYYY");
 
-  romeCityActualTime.innerHTML = romeTzTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    let romeCityActualTime = document.querySelector(".time");
 
-  let parisCityDate = document.querySelector("#par");
-  let parisTzTime = moment().tz("Europe/Paris");
+    romeCityActualTime.innerHTML = romeTzTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-  parisCityDate.innerHTML = parisTzTime.format("dddd, MMMM Do, YYYY");
+  let parCt = document.querySelector("#paris");
+  if (parCt) {
+    let parisCityDate = document.querySelector("#par");
+    let parisTzTime = moment().tz("Europe/Paris");
 
-  let parisActualTime = document.querySelector("#pid");
-  parisActualTime.innerHTML = parisTzTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    parisCityDate.innerHTML = parisTzTime.format("dddd, MMMM Do, YYYY");
+
+    let parisActualTime = document.querySelector("#pid");
+    parisActualTime.innerHTML = parisTzTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function changeCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector(".allCities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 countryDateTime();
 setInterval(countryDateTime, 1000);
+
+let searchCity = document.querySelector("#ch-city");
+searchCity.addEventListener("change", changeCity);
